ce8afa474707561588cd655615f9dd0e29dcb54b
cuneyt.corbaci <ccorbaci@gmail.com>
2024-02-28 18:04:05 +0300

    new file:   102_1_Design_hdd_ layout.sh

---
+++ 102_1_Design_hdd_ layout.sh
@@ -0,0 +1,23 @@
+#Unix directories
+    #This might be your enlightening moment in your Linux journey. Understanding    
+    
+#Filesystem Hierarchy Standard (FHS)
+-----------------------------------------------------------------
+| Directory | Description                                       |
+| ---       | ---                                               |
+| bin       | Essential command binaries                        |
+| boot      | Static files of the boot loader                   |
+| dev       | Device files                                      |
+| etc       | Host-specific system configuration                |
+| home      | Home directory of the users                       |
+| lib       | Essential shared libraries and kernel modules     |
+| media     | Mount point for removable media                   |
+| mnt       | Mount point for mounting a filesystem temporarily |
+| opt       | Add-on application software packages              |
+| root      | Home directory of the root user                   |
+| sbin      | Essential system binaries                         |
+| srv       | Data for services provided by this system         |
+| tmp       | Temporary files, sometimes purged on each boot    |
+| usr       | Secondary hierarchy                               |
+| var       | Variable data (logs, ...)                         |
+-----------------------------------------------------------------
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 101_1_Determine and Configure Hardware Settings
 101_2_Boot the System
 101_3_Change runlevels / boot targets and shutdown or reboot the system
+102.1 Design hard disk layout
